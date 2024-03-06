980f8e1ccb50f9368365fd6b3ddfcd25632b9bee
sharjeel <syedsherjeelali888@gmail.com>
2024-03-06 15:22:19 +0500

    blog36

--- blog_project2/config/database.js
+++ blog_project2/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST", "192.168.4.167"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi_blog"),
+      database: env("DATABASE_NAME", "postgres"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "dxtx998"),
+      password: env("DATABASE_PASSWORD", "postgres"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       // ssl: {
       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
